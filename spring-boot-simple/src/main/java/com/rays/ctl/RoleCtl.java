40ef0e860c93e015b8b5fd61b3bea9ccb9871c27
abhi070717 <abhibhawsar066@gmail.com>
2026-07-30 19:40:54 +0530

    RoleCtl search

--- spring-boot-simple/src/main/java/com/rays/ctl/RoleCtl.java
+++ spring-boot-simple/src/main/java/com/rays/ctl/RoleCtl.java
@@ -1,5 +1,7 @@
 package com.rays.ctl;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rays.common.ORSResponse;
@@ -95,5 +98,27 @@ public class RoleCtl extends BaseCtl {
 		}
 
 		return res;
+	}
+
+//	http://localhost:8080/Role/search/pageNo
+	@RequestMapping(value = "search/{pageNo}", method = { RequestMethod.POST, RequestMethod.GET })
+	public ORSResponse search(@RequestBody RoleForm form, @PathVariable int pageNo) {
+
+		RoleDTO dto = (RoleDTO) form.getDTO();
+		ORSResponse res = new ORSResponse();
+
+		int pageSize = 5;
+
+		List<RoleDTO> list = service.search(dto, pageNo, pageSize);
+
+		if (list != null) {
+			res.addData(list);
+			res.setSuccess(true);
+		} else {
+			res.addMessage("record not found");
+		}
+
+		return res;
+
 	}
 }
